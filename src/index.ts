8648f7d86816f18604299632830074ff95982a44
Mauricio Luiz de toledo <mauricioluiz3800@gmail.com>
2023-01-03 13:46:50 -0300

    Endpoint turma,type e app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app"
-import getAllDocentes from "./endpoints/getAllDocentes"
+import getAllDocentes from "./endpoints/getAllDocentes";
+import getAllTurma from "./endpoints/getAllTurma"
+// endpoint
 
-
+app.get("/turma", getAllTurma);
 
 app.get("/docente", getAllDocentes)
