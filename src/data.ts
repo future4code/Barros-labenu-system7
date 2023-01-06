08f4b2a15ad06f9c16e46282d9214ae91caaded7
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2023-01-06 11:19:51 -0300

    Erro de  servidor interno

--- src/data.ts
+++ src/data.ts
@@ -7,5 +7,20 @@ export const docentes: docente[] = [
         email: "beatriz_timoia@uorak.com",
         data_nasc: 1972-0o4-0o5,
         turma_id: 0o1
+    }, {
+        id: 0o2,
+        nome: "Felipe de Oliveira",
+        email: "felipe_oliveira@uorak.com",
+        data_nasc: 1974-0o2-12,
+        turma_id: 0o2
+      }, {
+        id: 0o3,
+        nome: "Alice Medeiros",
+        email: "alice_medeiros@uorak.com",
+        data_nasc: 1980-0o3-0o7,
+        turma_id: 0o3
     }
 ]
+    
+    
+    
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app"
+import createDocente from "./endpoints/createDocente"
 import getAllDocentes from "./endpoints/getAllDocentes"
 
 
 
 app.get("/docente", getAllDocentes)
+app.post("/docente", createDocente)
