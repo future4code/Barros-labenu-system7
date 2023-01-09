ef8db4287668b05c42068df453635798193068d3
Ariane Lopes de Camargo <arianee.lopees@hotmail.com>
2023-01-09 00:34:26 -0300

    sistem3

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import app from "./app"
+import createDocente from "./endpoints/createDocente"
 import createEstudante from "./endpoints/createEstudante"
 import getAllDocentes from "./endpoints/getAllDocentes"
 import getAllEstudantes from "./endpoints/getAllEstudantes"
 import getAllTurma from "./endpoints/getAllTurma"
 
-app.get("/docente", getAllDocentes)
 
+
+app.get("/docente", getAllDocentes)
+app.put("/docente", createDocente)
 
 app.get("/turma", getAllTurma)
 
 app.get("/estudante", getAllEstudantes)
+
 app.put("/estudante", createEstudante)
